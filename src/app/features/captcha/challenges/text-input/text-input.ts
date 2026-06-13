2a60384ed8b6f84d1f5678e47fc8ba1fed319b99
myacoubi <amineamoune904@gmail.com>
2026-06-13 15:40:08 +0100

    feat(text-input): initialize form control and split text target into characters in text-input component

--- src/app/features/captcha/challenges/text-input/text-input.ts
+++ src/app/features/captcha/challenges/text-input/text-input.ts
@@ -1,13 +1,25 @@
-import { Component, input, output } from "@angular/core";
-import { Challenge } from "../../../../core/models/challenge";
+import { Component, input, OnInit, output } from '@angular/core';
+import { Challenge } from '../../../../core/models/challenge';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-text-input', 
-    templateUrl: 'text-input.html',
-    styleUrl: 'text-input.html',
+  selector: 'app-text-input',
+  templateUrl: 'text-input.html',
+  styleUrl: 'text-input.html',
 })
-export class TextInputComponent {
-    challenge = input.required<Challenge>();
-    showError = input<boolean>(false);
-    answerChange = output<string>();
+export class TextInputComponent implements OnInit {
+  challenge = input.required<Challenge>();
+  showError = input<boolean>(false);
+  answerChange = output<string>();
+
+  control = new FormControl('');
+  chars: string[] = [];
+
+  ngOnInit(): void {
+    this.chars = this.challenge().textTarget!.split('');
+  }
+
+  onInput(): void {
+    this.answerChange.emit(this.control.value ?? '');
+  }
 }
