32b4a11617e770b8da61a2573c7c620fde784bbe
myacoubi <amineamoune904@gmail.com>
2026-06-13 15:06:45 +0100

    feat(mathComponent): component logic and template

---
+++ src/app/features/captcha/challenges/math/math.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from '@angular/core';
+import { Challenge } from '../../../../core/models/challenge';
+
+@Component({
+  selector: 'app-math-challenge',
+  templateUrl: 'math.html',
+  styleUrl: 'math.css',
+})
+export class MathChallengeComponent {
+  challenge = input.required<Challenge>();
+  showError = input(false);
+  answerChange = output<number>();
+
+  selectedAnswer: number | null = null;
+
+  selectOption(value: number): void {
+    this.selectedAnswer = value;
+    this.answerChange.emit(value);
+  }
+}
