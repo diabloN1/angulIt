3a21c59c76513f43df2bb32618b82bc0c2118839
myacoubi <amineamoune904@gmail.com>
2026-07-14 23:31:17 +0100

    feat: enhance image select challenge with valid/invalid state management

--- src/app/features/captcha/challenges/image-select/image-select.ts
+++ src/app/features/captcha/challenges/image-select/image-select.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, OnInit, output, signal, WritableSignal } from '@angular/core';
 import { Challenge } from '../../../../core/models/challenge';
 
 @Component({
@@ -6,22 +6,38 @@ import { Challenge } from '../../../../core/models/challenge';
   templateUrl: 'image-select.html',
   styleUrl: 'image-select.css',
 })
-export class ImageSelectComponent {
+export class ImageSelectComponent implements OnInit {
   challenge = input.required<Challenge>();
   showError = input(false);
+  showInvalid = input(false);
   answerChange = output<number[]>();
 
-  selectedIds: number[] = [];
+  selectedIds: WritableSignal<number[]> = signal([]);
+
+  constructor() {
+    effect(() => {
+      const isInvalid = this.showInvalid();
+      if (isInvalid) {
+        this.selectedIds.set([]);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    const storedAnswer = this.challenge().userAnswer;
+
+    if (Array.isArray(storedAnswer) && storedAnswer.every((item) => typeof item === 'number')) {
+      this.selectedIds.set(storedAnswer);
+    }
+  }
 
   toggleImage(id: number): void {
-    const idx = this.selectedIds.indexOf(id);
-    this.selectedIds =
-      idx === -1 ? [...this.selectedIds, id] : this.selectedIds.filter((i) => i !== id);
-    this.answerChange.emit(this.selectedIds);
+    const idx = this.selectedIds().indexOf(id);
+    this.selectedIds.update((old) => (idx === -1 ? [...old, id] : old.filter((i) => i !== id)));
+    this.answerChange.emit(this.selectedIds());
   }
 
   isSelected(id: number): boolean {
-    return this.selectedIds.includes(id);
+    return this.selectedIds().includes(id);
   }
 }
-
